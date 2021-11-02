cfa3a6f32a3e553f75eb9ce2ac9da614926f2286
elisa <elisa.bianchi@epfl.ch>
2021-11-02 18:48:17 +0100

    clean up + established classes

--- touchscreen_ws/src/reachy_touchscreen/screen_calibration.py
+++ touchscreen_ws/src/reachy_touchscreen/screen_calibration.py
@@ -1,33 +1,40 @@
 import numpy as np
 
-#TODO : change and use reachy hand for calibration
-s_a, s_b, s_c, t_a, t_b, t_c, z_a, z_b, z_c = 0.2
+class Screen_Calibrator : 
 
-A = np.array([
-  [1, 0, 0, s_a],
-  [0, 1, 0, t_a],  
-  [0, 0, 1, z_a],
-  [0, 0, 0, 1],  
-])
-B = np.array([
-  [1, 0, 0, s_b],
-  [0, 1, 0, t_b],  
-  [0, 0, 1, z_b],
-  [0, 0, 0, 1],  
-])
-C = np.array([
-  [1, 0, 0, s_c],
-  [0, 1, 0, t_c],  
-  [0, 0, 1, z_c],
-  [0, 0, 0, 1],  
-])
-
-def calibrate_screen():
+  """
+  initiating the calibration to use the screen in all conditions (z axis not covered, screen must lay flat)
+  """
+  def calibrate_screen():
     #TODO : implement forward kinematics to get motors --> to coord 
     #TODO : figure out how to get the 3 motor thingies
+    #TODO : change and use reachy hand for calibration
+    s_a, s_b, s_c, t_a, t_b, t_c, z_a, z_b, z_c = 0.2
+
+    A = np.array([
+      [1, 0, 0, s_a],
+      [0, 1, 0, t_a],  
+      [0, 0, 1, z_a],
+      [0, 0, 0, 1],  
+    ])
+    B = np.array([
+      [1, 0, 0, s_b],
+      [0, 1, 0, t_b],  
+      [0, 0, 1, z_b],
+      [0, 0, 0, 1],  
+    ])
+    C = np.array([
+      [1, 0, 0, s_c],
+      [0, 1, 0, t_c],  
+      [0, 0, 1, z_c],
+      [0, 0, 0, 1],  
+    ])
+
     coord_a = np.array[A[0, 3], A[1 ,3]]
     coord_b = np.array[B[0, 3], B[1 ,3]]
     coord_c = np.array[C[0, 3], C[1 ,3]]
+
+
 
 
     #fixed z position for touching the screen (assumes the screen is laying flat)
@@ -44,8 +51,8 @@ def calibrate_screen():
     rotation_matrix = [
         [np.cos(theta), np.sin(theta)], 
         [-np.sin(theta), np.cos(theta)]
-       ]
-    
+      ]
+      
     r_B = np.matmul(rotation_matrix, t_B)
     r_C = np.matmul(rotation_matrix, t_C)
 
@@ -56,6 +63,6 @@ def calibrate_screen():
 
     translation_matrix_2 = np.array[0, screen_width/2]
 
-    return [fixed_z, translation_matrix_1, rotation_matrix, translation_matrix_2, screen]
+    return [screen, fixed_z, np.linalg.inv(translation_matrix_1), np.linalg.inv(rotation_matrix), np.linalg.inv(translation_matrix_2)]
 
 
--- touchscreen_ws/src/reachy_touchscreen/screen_touch.py
+++ touchscreen_ws/src/reachy_touchscreen/screen_touch.py
@@ -1,19 +1,23 @@
 from reachy_sdk import ReachySDK
-import reachy_sdk
 from reachy_sdk.trajectory import goto
 from reachy_sdk.trajectory.interpolation import InterpolationMode
 import time
 import numpy as np
-import screen_processing
+from screen_calibration import Screen_Calibrator
+from screen_processing import Screen_Processing
+
+#TODO : all the links with reachy (--> figure how to get position by pressing a key) 
+#TODO : all the links with the screen subscriber
 
 reachy = ReachySDK(host='127.0.0.1')
 
 REST_COORD = np.array([
   [1, 0, 0, 0.1],
   [0, 1, 0, -0.1],  
-  [0, 0, 1, -0.1],
+  [0, 0, 1, -0.2],
   [0, 0, 0, 1],  
 ])
+
 
 
 # just to avoid rewriting the same code over and over
@@ -42,15 +46,28 @@ def look_at_hand(goal_coord):
 
 def main(args=None):
   
-  #reachy.turn_on('reachy')
-  #joint_goto(REST_COORD)
-  goal_coords = screen_processing.rescale_destination(150, 500)
-  print(goal_coords)
-  #look_at_hand(goal_coords)
-  #joint_goto(goal_coords)
-  #time.sleep(1.0)
-  #joint_goto(REST_COORD)
-  reachy.turn_off_smoothly('reachy')
+  # faire un screen calibrator
+  calibrator = Screen_Calibrator().calibrate_screen
+  # creer un nouveau screen processing avec les valeurs de la calibration
+  processor = Screen_Processing(calibrator[0], calibrator[1])
+  # puis un while true et ca tourne en boucle
+
+  #TODO : fix this with the destination goal + recieve info from screen
+  goal_coords = np.array([150, 500])
+  new_coords = np.array([150, 500])
+
+  while("screen_subscriber = true"):
+    if(new_coords != goal_coords):
+      goal_coords = processor.rescale_destination_pixels_to_meters(new_coords)
+      goal_coords = processor.rescale_destination_to_calibration(goal_coords, calibrator[2], calibrator[3], calibrator[4])
+      goal_matrix = processor.vector_to_matrix_press(goal_coords)
+      #reachy.turn_on('reachy')
+      #joint_goto(REST_COORD)
+      #joint_goto(goal_coords)
+      #time.sleep(1.0)
+      #joint_goto(REST_COORD)
+      reachy.turn_off_smoothly('reachy')
+
 
 if __name__ == '__main__':
     main()
